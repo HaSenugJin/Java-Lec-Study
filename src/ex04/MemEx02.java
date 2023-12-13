515cb6c8ee116c1763c49b946afa07b797d10b14
egdg4587 <egdg7777@gmail.com>
2023-12-13 14:37:47 +0900

    heap 공부

--- src/ex04/MemEx02.java
+++ src/ex04/MemEx02.java
@@ -1,11 +1,24 @@
 package ex04;
 
 class Person2 {
-
+    int age = 10;
+    char gender = '남';
 }
 
 public class MemEx02 {
     public static void main(String[] args) {
+        System.out.println("1년 지남");
+        System.out.println("2년 지남");
+        System.out.println("3년 지남");
 
+        //new = 동적할당
+        //heap에 띄움
+        Person2 p = new Person2();
+
+        //출력
+        System.out.println(p.age);
+        System.out.println(p.gender);
+
+        System.out.println("main 종료");
     }
 }
