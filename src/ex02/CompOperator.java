561605c40074c6b1c0bd41f9dd2fc0f7adf82303
egdg4587 <egdg7777@gmail.com>
2023-12-13 15:02:31 +0900

    연산자

---
+++ src/ex02/CompOperator.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class CompOperator {
+    public static void main(String[] args) {
+        System.out.println((3 == 4) + " ");//false
+        System.out.println((3 != 4) + " ");//true
+        System.out.println((3 > 4) + " ");//false
+        System.out.println((4 > 3) + " ");//ture
+
+        System.out.println((3 == 4 && 4 == 7) + " ");//false
+        System.out.println((3 != 4) + " ");//true
+    }
+}
