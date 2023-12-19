f4f59232839b4ac549dea96aff3b8af8f12cefe4
egdg4587 <egdg7777@gmail.com>
2023-12-19 16:39:55 +0900

    Scope

---
+++ src/ex04/test/ScopeEx01.java
@@ -0,0 +1,25 @@
+package ex04.test;
+
+public class ScopeEx01 {
+
+    int n1 = 1;
+    static int n2 = 2;
+
+    static void m1() {
+        int n1 = 10;
+        System.out.println("m1 : " + n1);
+    }
+
+    void m2() {
+        System.out.println("m2 : " + n1);
+    }
+
+    //메인 시작전에 static에 n2 변수와 m1메서드가 로드 되어 있음.
+    public static void main(String[] args) {
+        System.out.println("1");
+        m1();
+        System.out.println("2");
+        ScopeEx01 s = new ScopeEx01();
+        System.out.println(s.n1);
+    }
+}
