22211961f0b346b5b6ad2e1a929b63a369e99754
egdg4587 <egdg7777@gmail.com>
2023-12-20 11:34:02 +0900

    구구단리팩토리

---
+++ src/ex03/GuGudanEx04.java
@@ -0,0 +1,17 @@
+package ex03;
+
+class GugudanUtil {
+    static void gugudan(int x) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(x + "*" + i + " = " + (x * i));
+        }
+        System.out.println();
+    }
+}
+
+public class GuGudanEx04 {
+    public static void main(String[] args) {
+
+        GugudanUtil.gugudan(3);
+    }
+}
