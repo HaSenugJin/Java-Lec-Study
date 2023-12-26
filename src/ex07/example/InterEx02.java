1aa1bd577f32b260b86d113d727850372496b689
egdg4587 <egdg7777@gmail.com>
2023-12-26 16:40:40 +0900

    자바GUI

--- src/ex07/example/InterEx02.java
+++ src/ex07/example/InterEx02.java
@@ -1,2 +1,20 @@
-package ex07.example;public class InterEx02 {
+package ex07.example;
+
+interface EventListener {
+    void action();
+}
+
+class MyApp {
+    public void click(EventListener l) {
+        l.action();
+    }
+}
+
+public class InterEx02 {
+    public static void main(String[] args) {
+        MyApp app = new MyApp();
+        app.click(() -> {
+            System.out.println("회원가입 로직이 실행됩니다.");
+        });
+    }
 }
