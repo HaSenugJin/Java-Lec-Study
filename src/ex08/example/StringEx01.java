a38f45905e9e029af1dcf57ce4d858d157247b8b
egdg4587 <egdg7777@gmail.com>
2023-12-27 17:47:06 +0900

    string

--- src/ex08/example/StringEx01.java
+++ src/ex08/example/StringEx01.java
@@ -3,7 +3,10 @@ package ex08.example;
 public class StringEx01 {
 
     public static String encoding(String data) {
-        return "";
+        int count;
+
+
+        return data;
     }
 
     public static String decoding(String encodingData) {
@@ -14,6 +17,20 @@ public class StringEx01 {
         String data = "AABBBCCCCDDDDD"; // Byte -> 14Byte
 
         // 인코딩 A2B3C4D5
+        int a = data.indexOf("A");
+        int b = data.indexOf("B");
+        int c = data.indexOf("C");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        //if(data.equals("A"))
+
+
+//        for (int i = 0; i < data.length(); i++) {
+//            System.out.println(data.indexOf("A"));
+//        }
 
         // 디코딩 AABBBCCCCDDDDD
     }
