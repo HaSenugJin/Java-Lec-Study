cd7830334bd6bd496eb6f753e14fa772f6927c1d
egdg4587 <egdg7777@gmail.com>
2024-01-09 14:29:58 +0900

    단방향터통신

--- src/ex17/oneway/Client.java
+++ src/ex17/oneway/Client.java
@@ -1,2 +1,29 @@
-package ex17.oneway;public class Client {
+package ex17.oneway;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client { // 요청
+    public static void main(String[] args) {
+        // localhost = 127.0.0.1 (루프백)
+        try {
+            Socket socket = new Socket("127.0.0.1", 10000);
+
+            // 보조 스트림 : socket.getOutputStream()
+            // 버퍼 : OutputStreamWriter
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(),
+                            "UTF-8")
+            );
+
+            bw.write("msg1" + "\n");
+            bw.write("msg2" + "\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
