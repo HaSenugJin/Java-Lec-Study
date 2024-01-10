58659fc6773ec030c67c5cff45c0344ad8ac5473
egdg4587 <egdg7777@gmail.com>
2024-01-10 15:41:14 +0900

    전이중2단계

--- src/ex17/multi/Client.java
+++ src/ex17/multi/Client.java
@@ -1,44 +1,44 @@
 package ex17.multi;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            // 1. 소켓과 버퍼 만들기
-            // 127.0.0.1 = localhost
-            Socket socket = new Socket("localhost", 20000);
-            Scanner sc = new Scanner(System.in);
-            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 30000);
 
-            // 2. 메시지 전송 스레드
-            new Thread(() -> {
+        new Thread(() -> {
+            try {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), "UTF-8")
+                );
+
                 while (true) {
-                    String keyboardMsg = sc.nextLine();
-                    pw.println(keyboardMsg);
+                    String requestMsg = br.readLine();
+
+                    System.out.println(requestMsg);
                 }
-            }).start();
 
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(), "UTF-8")
-            );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
-            // 3. 메시지 읽기 스레드
-            new Thread(() -> {
+        new Thread(() -> {
+            try {
                 while (true) {
-                    try {
-                        String requestMsg = br.readLine();
-                        System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    Scanner sc = new Scanner(System.in);
+                    String responseMsg = sc.nextLine();
+                    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                    pw.println(responseMsg);
                 }
-            }).start();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 }
--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -9,42 +9,38 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(20000);
-            Socket socket = serverSocket.accept();
-            // 1. 소켓 연결 완료됨
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(30000);
+        Socket socket = serverSocket.accept();
 
-            // 버퍼 생성
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(), "UTF-8")
-            );
+        new Thread(() -> {
+            try {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), "UTF-8")
+                );
 
-            // 2. 메시지 받기 스레드
-            new Thread(() -> {
                 while (true) {
-                    try {
-                        String requestMsg = br.readLine();
-                        System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    String requestMsg = br.readLine();
+
+                    System.out.println(requestMsg);
                 }
-            }).start();
 
-            Scanner sc = new Scanner(System.in);
-            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
 
-            // 3. 메시지 전송 스레드
-            new Thread(() -> {
+        new Thread(() -> {
+            try {
                 while (true) {
-                    String keyboardMsg = sc.nextLine();
-                    pw.println(keyboardMsg);
+                    Scanner sc = new Scanner(System.in);
+                    String responseMsg = sc.nextLine();
+                    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                    pw.println(responseMsg);
                 }
-            }).start();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 }
