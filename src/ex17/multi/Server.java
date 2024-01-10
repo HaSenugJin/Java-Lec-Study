83431bb72b5dd2b0af63dfbe26580aaf3e2a1cbc
egdg4587 <egdg7777@gmail.com>
2024-01-10 15:28:02 +0900

    전이중통신-1단계

---
+++ src/ex17/multi/Server.java
@@ -0,0 +1,40 @@
+package ex17.multi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(20000);
+            Socket socket = serverSocket.accept();
+            // 소켓 연결 완료됨
+
+            // 버퍼 생성
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), "UTF-8")
+            );
+
+            // 메시지 받기 스레드
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String requestMsg = br.readLine();
+                        System.out.println("클라이언트로부터 받은 메시지 : " + requestMsg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
